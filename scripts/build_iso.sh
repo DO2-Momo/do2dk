f4803dc36dfaadb329c49c332198ce57eee3b298
vlev <matiasvlevi@gmail.com>
2023-04-07 03:31:30 +0000

    comments

--- scripts/build_iso.sh
+++ scripts/build_iso.sh
@@ -1,12 +1,14 @@
-
 ISO_DIR=./do2iso
 WORK_DIR=./tmp
 BUILD_DIR=./dist
 
+# Create work directory
 if [ ! -d $WORK_DIR ]; then
     mkdir $WORK_DIR
 fi
 
+# Build ISO image
 mkarchiso -v -w $WORK_DIR -o $BUILD_DIR $ISO_DIR
 
+# Clean work directory
 rm -rf $WORK_DIR
