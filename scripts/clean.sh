5c3724f6ad8b21846bc515cced468cac124355c0
vlev <matiasvlevi@gmail.com>
2023-04-07 03:31:44 +0000

    comments in clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,15 @@
 # Installer related
-rm -rf ./yarp/target/
+
+# Remove build directory in source module
+rm -rf ./yarp/target
+
+# Remove build directory in destination
 rm -rf ./do2iso/airootfs/root/.yarp
 
 # ISO Related
+
+# Remove working directory
 rm -rf ./tmp
+
+# Remove distributable directory
 rm -rf ./dist/
