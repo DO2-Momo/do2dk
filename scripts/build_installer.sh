ab854d66184fe84b92ea28653284a1a2af0dc40e
vlev <matiasvlevi@gmail.com>
2023-04-07 03:31:25 +0000

    path variables

--- scripts/build_installer.sh
+++ scripts/build_installer.sh
@@ -1,10 +1,22 @@
-cd ./yarp; cargo build -r; cd ..;
+ISO_PATH=./do2iso
 
-mkdir ./do2iso/airootfs/root/.yarp/
+INSTALLER_SRC=./yarp
+INSTALLER_DEST=$ISO_PATH/airootfs/root/.yarp
+
+# Build Installer
+cd $INSTALLER_SRC; cargo build -r; cd ..;
+
+# Clean previous installer destination directory 
+if [ -d $INSTALLER_DEST ]; then
+    rm -rf $INSTALLER_DEST
+fi
+
+# Create destination directiory
+mkdir $INSTALLER_DEST
 
 echo "Copying Installer configurations... "
-cp -r ./yarp/packages ./do2iso/airootfs/root/.yarp/packages
-cp -r ./yarp/root     ./do2iso/airootfs/root/.yarp/root
+cp -r $INSTALLER_SRC/packages $INSTALLER_DEST/packages
+cp -r $INSTALLER_SRC/root     $INSTALLER_DEST/root
 
 echo "Copying Installer executable... "
-cp ./yarp/target/release/rs-yarp ./do2iso/airootfs/root/.yarp/rs-yarp
+cp $INSTALLER_SRC/target/release/rs-yarp $INSTALLER_DEST/rs-yarp
